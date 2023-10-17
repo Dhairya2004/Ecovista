73a5bfc3d46413d63e7e9808439ebaa38645866c
Dhairya2004 <guptadhairya38@gmail.com>
2023-10-17 20:23:27 +0530

    Refactor leaderboard page and add leaderboard table
    - Refactored the leaderboard page to improve the layout and styling
    - Added a leaderboard table to display the top users and their coin points
    - Updated the leaderboard table with actual data fetched via firebase

--- Auth/test-auth.js
+++ Auth/test-auth.js
@@ -1,14 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-import { getFirestore, doc, updateDoc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  updateDoc,
+  getDoc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDN90YYc-_CdzJE0q5KTXnyK9KeA2NirDs",
-    authDomain: "ecovista-68307.firebaseapp.com",
-    projectId: "ecovista-68307",
-    storageBucket: "ecovista-68307.appspot.com",
-    messagingSenderId: "273629603323",
-    appId: "1:273629603323:web:035ede64ae376506f0d75e"
+  apiKey: "AIzaSyDN90YYc-_CdzJE0q5KTXnyK9KeA2NirDs",
+  authDomain: "ecovista-68307.firebaseapp.com",
+  projectId: "ecovista-68307",
+  storageBucket: "ecovista-68307.appspot.com",
+  messagingSenderId: "273629603323",
+  appId: "1:273629603323:web:035ede64ae376506f0d75e",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -29,72 +41,74 @@ earnCoinsButton.style.display = "none";
 message.style.display = "none";
 
 const userSignIn = async () => {
-    signInWithPopup(auth, provider)
-        .then(async (result) => {
-            const user = result.user;
-            console.log(user);
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    console.log(user);
 
-            // Check if the user document already exists
-            const userDocRef = doc(db, "users", user.uid);
-            const userDoc = await getDoc(userDocRef);
+    // Check if the user document already exists
+    const userDocRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userDocRef);
 
-            if (!userDoc.exists()) {
-                // If the document doesn't exist, create it and initialize the coin balance
-                await setDoc(userDocRef, { coins: 0 });
-            }
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+    if (!userDoc.exists()) {
+      // If the document doesn't exist, create it and initialize the coin balance
+      const username = user.displayName || "Test-User"; // Use display name or a default if not available
+      await setDoc(userDocRef, { coins: 0, username: username });
+    }
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(`Error: ${errorCode}, ${errorMessage}`);
+  }
 };
 
-
 const userSignOut = async () => {
-    signOut(auth).then(() => {
-        alert("You have signed out successfully!");
-    }).catch((error) => { });
+  signOut(auth)
+    .then(() => {
+      alert("You have signed out successfully!");
+    })
+    .catch((error) => {});
 };
 
 const addCoins = async (amount) => {
-    const user = auth.currentUser;
-    if (user) {
-        const userDocRef = doc(db, "users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-        if (userDoc.exists()) {
-            const currentCoins = userDoc.data().coins || 0;
-            const newCoins = currentCoins + amount;
-            await updateDoc(userDocRef, { coins: newCoins });
-            coinBalance.textContent = newCoins;
-        }
+  const user = auth.currentUser;
+  if (user) {
+    const userDocRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userDocRef);
+    if (userDoc.exists()) {
+      const currentCoins = userDoc.data().coins || 0;
+      const newCoins = currentCoins + amount;
+      await updateDoc(userDocRef, { coins: newCoins });
+      coinBalance.textContent = newCoins;
     }
+  }
 };
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-        signOutButton.style.display = "block";
-        earnCoinsButton.style.display = "block";
-        message.style.display = "block";
-        userName.textContent = user.displayName;
-        userEmail.textContent = user.email;
+  if (user) {
+    signOutButton.style.display = "block";
+    earnCoinsButton.style.display = "block";
+    message.style.display = "block";
+    userName.textContent = user.displayName;
+    userEmail.textContent = user.email;
 
-        // Load and display the user's coin balance
-        const userDocRef = doc(db, "users", user.uid);
-        getDoc(userDocRef).then((userDoc) => {
-            if (userDoc.exists()) {
-                const coins = userDoc.data().coins || 0;
-                coinBalance.textContent = coins;
-            }
-        });
-    } else {
-        signOutButton.style.display = "none";
-        earnCoinsButton.style.display = "none";
-        message.style.display = "none";
-    }
+    // Load and display the user's coin balance
+    const userDocRef = doc(db, "users", user.uid);
+    getDoc(userDocRef).then((userDoc) => {
+      if (userDoc.exists()) {
+        const coins = userDoc.data().coins || 0;
+        coinBalance.textContent = coins;
+      }
+    });
+  } else {
+    signOutButton.style.display = "none";
+    earnCoinsButton.style.display = "none";
+    message.style.display = "none";
+  }
 });
 
-signInButton.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignOut);
-earnCoinsButton.addEventListener('click', () => {
-    addCoins(10); // Add 10 coins when the button is clicked
+signInButton.addEventListener("click", userSignIn);
+signOutButton.addEventListener("click", userSignOut);
+earnCoinsButton.addEventListener("click", () => {
+  addCoins(10); // Add 10 coins when the button is clicked
 });
--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,14 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-import { getFirestore, doc, updateDoc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  updateDoc,
+  getDoc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDN90YYc-_CdzJE0q5KTXnyK9KeA2NirDs",
-    authDomain: "ecovista-68307.firebaseapp.com",
-    projectId: "ecovista-68307",
-    storageBucket: "ecovista-68307.appspot.com",
-    messagingSenderId: "273629603323",
-    appId: "1:273629603323:web:035ede64ae376506f0d75e"
+  apiKey: "AIzaSyDN90YYc-_CdzJE0q5KTXnyK9KeA2NirDs",
+  authDomain: "ecovista-68307.firebaseapp.com",
+  projectId: "ecovista-68307",
+  storageBucket: "ecovista-68307.appspot.com",
+  messagingSenderId: "273629603323",
+  appId: "1:273629603323:web:035ede64ae376506f0d75e",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,115 +28,186 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const db = getFirestore();
 
-const profilePicture = document.getElementById('profile-picture');
+const profilePicture = document.getElementById("profile-picture");
 
 const signInButton = document.getElementById("signInButton");
 
 const userSignIn = async () => {
-    signInWithPopup(auth, provider)
-        .then(async (result) => {
-            hideContainer();
-            addCoins(0);
-            fetchUserinfo();
-            const user = result.user;
-            console.log(user);
+  signInWithPopup(auth, provider)
+    .then(async (result) => {
+      hideContainer();
+      addCoins(0);
+      fetchUserinfo();
+      const user = result.user;
+      console.log(user);
 
-            // Check if the user document already exists
-            const userDocRef = doc(db, "users", user.uid);
-            const userDoc = await getDoc(userDocRef);
+      // Check if the user document already exists
+      const userDocRef = doc(db, "users", user.uid);
+      const userDoc = await getDoc(userDocRef);
 
-            if (!userDoc.exists()) {
-                // If the document doesn't exist, create it and initialize the coin balance
-                await setDoc(userDocRef, { coins: 0 });
-            }
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+      if (!userDoc.exists()) {
+        // If the document doesn't exist, create it and initialize the coin balance
+        const username = user.displayName || "Test-User"; // Use display name or a default if not available
+        await setDoc(userDocRef, { coins: 0, username: username });
+      }
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 };
 
 const userSignOut = async () => {
-    signOut(auth).then(() => {
-        showContainer();
-        profilePicture.src = "assets/img/profile.png";
-    }).catch((error) => { });
+  signOut(auth)
+    .then(() => {
+      showContainer();
+      updateLeaderboard();
+      profilePicture.src = "assets/img/profile.png";
+    })
+    .catch((error) => {});
 };
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-        hideContainer();
-        addCoins(0);
-        fetchUserinfo();
-        const userDocRef = doc(db, "users", user.uid);
-    }
-    else {
-        showContainer();
-    }
+  if (user) {
+    hideContainer();
+    addCoins(0);
+    fetchUserinfo();
+    const userDocRef = doc(db, "users", user.uid);
+  } else {
+    showContainer();
+  }
 });
 
-
-const container = document.querySelector('.container');
-const blurcontent = document.querySelector('.blur-content');
+const container = document.querySelector(".container");
+const blurcontent = document.querySelector(".blur-content");
 const signOutButton = document.getElementById("signOutButton");
 
 const coinBalance = document.getElementById("coinBalance");
 
-
-
 const addCoins = async (amount) => {
-    const user = auth.currentUser;
-    if (user) {
-        const userDocRef = doc(db, "users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-        if (userDoc.exists()) {
-            const currentCoins = userDoc.data().coins || 0;
-            const newCoins = currentCoins + amount;
-            await updateDoc(userDocRef, { coins: newCoins });
-            coinBalance.textContent = newCoins;
-        }
+  const user = auth.currentUser;
+  if (user) {
+    const userDocRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userDocRef);
+    if (userDoc.exists()) {
+      const currentCoins = userDoc.data().coins || 0;
+      const newCoins = currentCoins + amount;
+      await updateDoc(userDocRef, { coins: newCoins });
+      coinBalance.textContent = newCoins;
     }
+  }
 };
-
-
 
 // Function to hide the container
 function hideContainer() {
-    container.style.display = 'none';
-    blurcontent.style.display = 'none';
-    signOutButton.style.display = 'block';
+  container.style.display = "none";
+  blurcontent.style.display = "none";
+  signOutButton.style.display = "block";
+  document.body.style.overflow = "auto";
 }
 // Function to show the container
 function showContainer() {
-    blurcontent.style.display = 'block';
-    container.style.display = 'flex';
-    signOutButton.style.display = 'none';
+  blurcontent.style.display = "block";
+  container.style.display = "flex";
+  signOutButton.style.display = "none";
+  document.body.style.overflow = "hidden";
 }
 
-
-signInButton.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignOut);
+signInButton.addEventListener("click", userSignIn);
+signOutButton.addEventListener("click", userSignOut);
 
 const fetchUserinfo = async () => {
-    const user = auth.currentUser;
-    if (user) {
-        const displayName = user.displayName;
-        const photoURL = user.photoURL;
+  const user = auth.currentUser;
+  if (user) {
+    const displayName = user.displayName;
+    const photoURL = user.photoURL;
 
-
-        // Set the 'src' attribute to the 'photoURL'
-        profilePicture.src = photoURL;
-    }
+    // Set the 'src' attribute to the 'photoURL'
+    profilePicture.src = photoURL;
+  }
 };
 
 const profileContainer = document.querySelector(".profile-container");
 const signOutDiv = document.getElementById("signOutButton");
 
 function showSignOut() {
-    setTimeout(signOutDiv.classList.toggle("show"), 3000);
+  setTimeout(signOutDiv.classList.toggle("show"), 3000);
 }
 
-profileContainer.addEventListener('mouseover', showSignOut);
-signOutDiv.addEventListener('mouseover',showSignOut);
-signOutDiv.addEventListener('mouseout',showSignOut);
-profileContainer.addEventListener('mouseout', showSignOut);
+profileContainer.addEventListener("mouseover", showSignOut);
+signOutDiv.addEventListener("mouseover", showSignOut);
+signOutDiv.addEventListener("mouseout", showSignOut);
+profileContainer.addEventListener("mouseout", showSignOut);
+
+import {
+  getDocs,
+  collection,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+// ... Other imports and Firebase setup code ...
+// Function to fetch user data from Firestore and update the leaderboard
+const updateLeaderboard = async () => {
+    const leaderboardTable = document.getElementById("leaderboard-table");
+    const leaderboardTopTable = document.getElementById("leaderboard-top");
+  
+    // Logging to check if leaderboardTable is null
+    console.log("leaderboardTable:", leaderboardTable);
+  
+    // Clear existing rows from the tables
+    leaderboardTable.innerHTML = "";
+    leaderboardTopTable.innerHTML = "";
+  
+    try {
+      // Fetch all users from Firestore
+      const usersSnapshot = await getDocs(collection(db, "users"));
+      const users = [];
+  
+      usersSnapshot.forEach((doc) => {
+        users.push({
+          uid: doc.id,
+          ...doc.data(),
+        });
+      });
+  
+      // Sort users based on coins in descending order
+      users.sort((a, b) => b.coins - a.coins);
+  
+      // Array of trophy image paths
+      const trophyImages = [
+        "./assets/img/goldtrophy.webp",
+        "./assets/img/silvertrophy.png",
+        "./assets/img/bronzetrophy.png",
+      ];
+  
+      // Update the tables with sorted user data
+      users.forEach((user, index) => {
+        const tr = document.createElement("tr");
+        
+        // Add class "special" to the tr of the user with the second rank
+        if (index === 1) {
+          tr.classList.add("special");
+        }
+        
+        tr.innerHTML = `
+          <td class="number">${index + 1}</td>
+          <td class="name">${user.username}</td>
+          <td class="points">
+            ${user.coins}
+            ${index < trophyImages.length ? `<img class="gold-medal" src="${trophyImages[index]}" alt="trophy" />` : ''}
+          </td>
+        `;
+  
+        if (index === 0) {
+          // Top-ranked user goes to leaderboard-top
+          leaderboardTopTable.appendChild(tr);
+        } else {
+          // Other users go to leaderboard-table
+          leaderboardTable.appendChild(tr);
+        }
+      });
+    } catch (error) {
+      console.error("Error updating leaderboard:", error);
+    }
+  };
+  
+  // Call the updateLeaderboard function on page load
+  window.addEventListener("load", updateLeaderboard);
